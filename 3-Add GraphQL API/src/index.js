c296b1596191850c63b187bb73f2f9a09a3a56a7
amitznati-work-hp <amitznati@gmail.com>
2021-01-31 21:33:52 +0200

    3-Add GraphQL API

---
+++ 3-Add GraphQL API/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// Amplify
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+
+Amplify.configure(config);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
